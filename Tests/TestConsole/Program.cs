67c8eeebb4cda7e5d4f26e7a57cde0e77d4f7c61
dubsent <dubsent@gmail.com>
2020-03-07 18:41:10 +0300

    Добавлена сотня игроков

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Gamer gamer = new Gamer();
+            gamer.Name = "Игрок 1";
+            gamer.DayOfBirthday = new DateTime(1987, 6, 16, 3, 15, 35);
+
+            Gamer[] gamers = new Gamer[100];
+            for (var i = 0; i < gamers.Length; i++)
+            {
+                var g = new Gamer();
+                //g.Name = $"Gamer {i + 1}";
+                g.Name = string.Format("Gamer {0}", i + 1);
+                g.DayOfBirthday = DateTime.Now;
+                gamers[i] = g;
+            }
+
+            Console.ReadLine();
         }
+    }
+
+    class Gamer
+    {
+        public string Name;
+        public DateTime DayOfBirthday;
     }
 }
