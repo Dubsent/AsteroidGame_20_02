13728e5eb18db1e6d46986df1444015b7e0c275c
dubsent <dubsent@gmail.com>
2020-03-07 21:14:10 +0300

    Рефакторинг

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -10,27 +10,35 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Gamer gamer = new Gamer();
-            gamer.Name = "Игрок 1";
-            gamer.DayOfBirthday = new DateTime(1987, 6, 16, 3, 15, 35);
+            Gamer gamer = new Gamer("Игрок 1", new DateTime(1987, 6, 16, 3, 15, 35));
 
             Gamer[] gamers = new Gamer[100];
             for (var i = 0; i < gamers.Length; i++)
             {
-                var g = new Gamer();
+                var g = new Gamer(string.Format("Gamer {0}", i + 1), DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18))));
                 //g.Name = $"Gamer {i + 1}";
-                g.Name = string.Format("Gamer {0}", i + 1);
-                g.DayOfBirthday = DateTime.Now;
                 gamers[i] = g;
             }
 
-            Console.ReadLine();
-        }
-    }
+            gamer.SayYourName();
 
-    class Gamer
-    {
-        public string Name;
-        public DateTime DayOfBirthday;
+
+            Console.WriteLine();
+
+            foreach (var g in gamers)
+            {
+                g.SayYourName();
+            }
+
+            //gamer.SetName("Dubsent");
+            //Console.WriteLine("Имя игрока {0}", gamer.GetName());
+
+            gamer.Name = "BowWow";
+            Console.WriteLine("Игрок {0}", gamer.Name);
+
+            Console.ReadLine();
+
+
+        }
     }
 }
